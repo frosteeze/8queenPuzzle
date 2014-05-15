9ee42190d56247450ba3921f1233021ab6a4a11f
Andre Supena <herapingomega@gmail.com>
2014-05-15 09:30:48 -0700

    Added hill climbing and genetic algorithm.

--- queenProject/src/queenproject/QueenProject.java
+++ queenProject/src/queenproject/QueenProject.java
@@ -16,12 +16,31 @@ public class QueenProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        queenPuzzle test = new queenPuzzle();
+        queenPuzzle [] test = new queenPuzzle[100];
+        System.out.println(test.length);
+        for(int i = 0; i < 100; i++){
+            test[i] = new queenPuzzle();
+        }
         
-        test.print();
-        
-        test.attackingQs();
-        System.out.println("Number of attacking queens: " + test.attackingQs());
+        int nSolved = 0;
+        int steps = 0;
+        long realTime = 0;
+        for(int i = 0; i < 100; i++){
+            long startTime = System.nanoTime();
+            steps = steps + test[i].hillClimb();
+            long endTime = System.nanoTime();
+            realTime = realTime + (endTime - startTime);
+            if(test[i].attackingQs() == 0) {
+                test[i].print();
+                nSolved++;
+            }
+            System.out.println("The number of attacking queens: " + test[i].attackingQs());
+        }
+        System.out.println("The number of solved problems is: " + nSolved);
+        System.out.println("The average cost is: " + (steps/100));
+        double seconds = (double)realTime/1000000000.0;
+        System.out.println("The average running time is: " + (seconds/100) + " seconds");
+                
     }
     
 }
